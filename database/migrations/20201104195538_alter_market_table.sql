4b2a3eadeeac94a1e1f085f70509632dc551f87c
Joe Sweeny <joesweeny@intelligencefusion.co.uk>
2020-11-04 21:04:27 +0000

    Update migration column removing not null

--- database/migrations/20201104195538_alter_market_table.sql
+++ database/migrations/20201104195538_alter_market_table.sql
@@ -1,7 +1,7 @@
 -- +goose Up
 -- +goose StatementBegin
 ALTER TABLE market
-ADD COLUMN id VARCHAR NOT NULL;
+ADD COLUMN id VARCHAR;
 
 ALTER TABLE market
 RENAME COLUMN exchange_market TO exchange_runners;
