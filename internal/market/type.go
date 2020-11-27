450941b55647effb9258eabe15c7c0d85eb61e2b
Joe Sweeny <joesweeny@intelligencefusion.co.uk>
2020-11-27 16:09:19 +0000

    Reorder types and remove helper functions

--- internal/market/type.go
+++ internal/market/type.go
@@ -1,22 +1,5 @@
 package market
 
-import (
-	"database/sql/driver"
-	"encoding/json"
-	"errors"
-)
-
-type OverUnderMarket struct {
-	ID        string    `json:"id"`
-	EventID   uint64    `json:"eventId"`
-	Name      string    `json:"name"`
-	Side      string    `json:"side"`
-	Exchange  string    `json:"exchange"`
-	Over      PriceSize `json:"over"`
-	Under     PriceSize `json:"under"`
-	Timestamp int64     `json:"timestamp"`
-}
-
 type BTTSMarket struct {
 	ID        string    `json:"id"`
 	EventID   uint64    `json:"eventId"`
@@ -40,17 +23,15 @@ type MatchOddsMarket struct {
 	Timestamp int64     `json:"timestamp"`
 }
 
-func (m OverUnderMarket) Value() (driver.Value, error) {
-	return json.Marshal(m)
-}
-
-func (m *OverUnderMarket) Scan(value interface{}) error {
-	b, ok := value.([]byte)
-	if !ok {
-		return errors.New("type assertion to []byte failed")
-	}
-
-	return json.Unmarshal(b, &m)
+type OverUnderMarket struct {
+	ID        string    `json:"id"`
+	EventID   uint64    `json:"eventId"`
+	Name      string    `json:"name"`
+	Side      string    `json:"side"`
+	Exchange  string    `json:"exchange"`
+	Over      PriceSize `json:"over"`
+	Under     PriceSize `json:"under"`
+	Timestamp int64     `json:"timestamp"`
 }
 
 type PriceSize struct {
