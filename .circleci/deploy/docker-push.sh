f19bca0d96adbec1befb79ea29b28662b4d62ad6
Joe Sweeny <joesweeny@intelligencefusion.co.uk>
2023-01-31 21:24:11 +0000

    Update docker image handling

--- .circleci/deploy/docker-push.sh
+++ .circleci/deploy/docker-push.sh
@@ -4,5 +4,5 @@ set -e
 
 aws ecr get-login --no-include-email --region $AWS_DEFAULT_REGION | bash
 
-docker tag "statistico-odds-warehouse_console" "$AWS_ECR_ACCOUNT_URL/statistico-odds-warehouse:latest"
+docker tag "statistico-odds-warehouse-console" "$AWS_ECR_ACCOUNT_URL/statistico-odds-warehouse:latest"
 docker push "$AWS_ECR_ACCOUNT_URL/statistico-odds-warehouse:latest"
