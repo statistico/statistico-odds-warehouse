fdc0170a79bcc46e4895accfcf6ea66c7f8f10ae
Joe Sweeny <joesweeny@intelligencefusion.co.uk>
2023-08-30 14:18:27 +0100

    Add grpc tests to stream markets

--- go.mod
+++ go.mod
@@ -27,7 +27,7 @@ require (
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
-	github.com/statistico/statistico-proto v0.0.9 // indirect
+	github.com/statistico/statistico-proto v0.1.24 // indirect
 	github.com/stretchr/objx v0.3.0 // indirect
 	golang.org/x/net v0.0.0-20210331212208-0fccb6fa2b5c // indirect
 	golang.org/x/sys v0.0.0-20210403161142-5e06dd20ab57 // indirect
--- internal/warehouse/grpc/market_test.go
+++ internal/warehouse/grpc/market_test.go
@@ -153,6 +153,256 @@ func TestMarketService_GetExchangeOdds(t *testing.T) {
 	})
 }
 
+func TestMarketService_GetEventMarkets(t *testing.T) {
+	t.Run("parses markets from reader and streams market structs", func(t *testing.T) {
+		t.Helper()
+
+		reader := new(MockMarketReader)
+		logger, hook := test.NewNullLogger()
+
+		service := g.NewMarketService(reader, logger)
+
+		server := new(MockMarketServer)
+
+		req := statistico.EventMarketRequest{
+			EventId:  15691981,
+			Market:   []string{"OVER_UNDER_25"},
+			Exchange: []string{"BETFAIR"},
+		}
+
+		markets := []*warehouse.Market{
+			{
+				ID:            "1.2345",
+				Name:          "OVER_UNDER_25",
+				EventID:       15691981,
+				CompetitionID: 8,
+				SeasonID:      17420,
+				EventDate:     time.Unix(1606824714, 0),
+				Exchange:      "BETFAIR",
+				Runners: []*warehouse.Runner{
+					{
+						ID:       1234,
+						MarketID: "1.2345",
+						Name:     "OVER",
+						BackPrice: &warehouse.Odds{
+							Value:     1.45,
+							Size:      3410.12,
+							Side:      "BACK",
+							Timestamp: time.Unix(1606824714, 0),
+						},
+						LayPrice: nil,
+					},
+					{
+						ID:       6789,
+						MarketID: "1.2345",
+						Name:     "UNDER",
+						BackPrice: &warehouse.Odds{
+							Value:     2.26,
+							Size:      3410.12,
+							Side:      "BACK",
+							Timestamp: time.Unix(1606824714, 0),
+						},
+						LayPrice: nil,
+					},
+				},
+			},
+		}
+
+		q := warehouse.MarketReaderQuery{
+			Market:   []string{"OVER_UNDER_25"},
+			Exchange: []string{"BETFAIR"},
+		}
+
+		reader.On("MarketsByEventID", req.EventId, &q).Return(markets, nil)
+
+		marketResponse := mock.MatchedBy(func(m *statistico.Market) bool {
+			a := assert.New(t)
+
+			a.Equal("1.2345", m.Id)
+			a.Equal("OVER_UNDER_25", m.Name)
+			a.Equal(uint64(15691981), m.EventId)
+			a.Equal(uint64(8), m.CompetitionId)
+			a.Equal(uint64(17420), m.SeasonId)
+			a.Equal(int64(1606824714), m.DateTime.Utc)
+			a.Equal("2020-12-01T12:11:54Z", m.DateTime.Rfc)
+			a.Equal("BETFAIR", m.Exchange)
+
+			a.Equal(uint64(1234), m.Runners[0].Id)
+			a.Equal("OVER", m.Runners[0].Name)
+			a.Equal(float32(1.45), m.Runners[0].BackOdds.Price)
+			a.Equal(float32(3410.12), m.Runners[0].BackOdds.Size)
+			a.Equal("BACK", m.Runners[0].BackOdds.Side)
+			a.Equal(uint64(1606824714), m.Runners[0].BackOdds.Timestamp)
+
+			a.Equal(uint64(6789), m.Runners[1].Id)
+			a.Equal("UNDER", m.Runners[1].Name)
+			a.Equal(float32(2.26), m.Runners[1].BackOdds.Price)
+			a.Equal(float32(3410.12), m.Runners[1].BackOdds.Size)
+			a.Equal("BACK", m.Runners[1].BackOdds.Side)
+			a.Equal(uint64(1606824714), m.Runners[1].BackOdds.Timestamp)
+			return true
+		})
+
+		server.On("Send", marketResponse).
+			Times(1).
+			Return(nil)
+
+		err := service.GetEventMarkets(&req, server)
+
+		if err != nil {
+			t.Fatalf("Expected nil, got %s", err.Error())
+		}
+
+		assert.Equal(t, 0, len(hook.Entries))
+		reader.AssertExpectations(t)
+		server.AssertExpectations(t)
+	})
+
+	t.Run("logs error if error return from market reader", func(t *testing.T) {
+		t.Helper()
+
+		reader := new(MockMarketReader)
+		logger, hook := test.NewNullLogger()
+
+		service := g.NewMarketService(reader, logger)
+
+		server := new(MockMarketServer)
+
+		req := statistico.EventMarketRequest{
+			EventId:  15691981,
+			Market:   []string{"OVER_UNDER_25"},
+			Exchange: []string{"BETFAIR"},
+		}
+
+		q := warehouse.MarketReaderQuery{
+			Market:   []string{"OVER_UNDER_25"},
+			Exchange: []string{"BETFAIR"},
+		}
+
+		reader.On("MarketsByEventID", req.EventId, &q).Return([]*warehouse.Market{}, errors.New("oh no"))
+
+		server.AssertNotCalled(t, "Send", mock.AnythingOfType("*statistico.Market"))
+
+		err := service.GetEventMarkets(&req, server)
+
+		if err == nil {
+			t.Fatal("Expected error, got nil")
+		}
+
+		assert.Equal(t, "rpc error: code = Internal desc = internal server error", err.Error())
+		assert.Equal(t, "error fetching markets from reader: oh no", hook.LastEntry().Message)
+		assert.Equal(t, 1, len(hook.Entries))
+		assert.Equal(t, logrus.ErrorLevel, hook.LastEntry().Level)
+		reader.AssertExpectations(t)
+	})
+
+	t.Run("logs error if error streaming Market", func(t *testing.T) {
+		t.Helper()
+
+		reader := new(MockMarketReader)
+		logger, hook := test.NewNullLogger()
+
+		service := g.NewMarketService(reader, logger)
+
+		server := new(MockMarketServer)
+
+		req := statistico.EventMarketRequest{
+			EventId:  15691981,
+			Market:   []string{"OVER_UNDER_25"},
+			Exchange: []string{"BETFAIR"},
+		}
+
+		markets := []*warehouse.Market{
+			{
+				ID:            "1.2345",
+				Name:          "OVER_UNDER_25",
+				EventID:       15691981,
+				CompetitionID: 8,
+				SeasonID:      17420,
+				EventDate:     time.Unix(1606824714, 0),
+				Exchange:      "BETFAIR",
+				Runners: []*warehouse.Runner{
+					{
+						ID:       1234,
+						MarketID: "1.2345",
+						Name:     "OVER",
+						BackPrice: &warehouse.Odds{
+							Value:     1.45,
+							Size:      3410.12,
+							Side:      "BACK",
+							Timestamp: time.Unix(1606824714, 0),
+						},
+						LayPrice: nil,
+					},
+					{
+						ID:       6789,
+						MarketID: "1.2345",
+						Name:     "UNDER",
+						BackPrice: &warehouse.Odds{
+							Value:     2.26,
+							Size:      3410.12,
+							Side:      "BACK",
+							Timestamp: time.Unix(1606824714, 0),
+						},
+						LayPrice: nil,
+					},
+				},
+			},
+		}
+
+		q := warehouse.MarketReaderQuery{
+			Market:   []string{"OVER_UNDER_25"},
+			Exchange: []string{"BETFAIR"},
+		}
+
+		marketResponse := mock.MatchedBy(func(m *statistico.Market) bool {
+			a := assert.New(t)
+
+			a.Equal("1.2345", m.Id)
+			a.Equal("OVER_UNDER_25", m.Name)
+			a.Equal(uint64(15691981), m.EventId)
+			a.Equal(uint64(8), m.CompetitionId)
+			a.Equal(uint64(17420), m.SeasonId)
+			a.Equal(int64(1606824714), m.DateTime.Utc)
+			a.Equal("2020-12-01T12:11:54Z", m.DateTime.Rfc)
+			a.Equal("BETFAIR", m.Exchange)
+
+			a.Equal(uint64(1234), m.Runners[0].Id)
+			a.Equal("OVER", m.Runners[0].Name)
+			a.Equal(float32(1.45), m.Runners[0].BackOdds.Price)
+			a.Equal(float32(3410.12), m.Runners[0].BackOdds.Size)
+			a.Equal("BACK", m.Runners[0].BackOdds.Side)
+			a.Equal(uint64(1606824714), m.Runners[0].BackOdds.Timestamp)
+
+			a.Equal(uint64(6789), m.Runners[1].Id)
+			a.Equal("UNDER", m.Runners[1].Name)
+			a.Equal(float32(2.26), m.Runners[1].BackOdds.Price)
+			a.Equal(float32(3410.12), m.Runners[1].BackOdds.Size)
+			a.Equal("BACK", m.Runners[1].BackOdds.Side)
+			a.Equal(uint64(1606824714), m.Runners[1].BackOdds.Timestamp)
+			return true
+		})
+
+		reader.On("MarketsByEventID", req.EventId, &q).Return(markets, nil)
+
+		server.On("Send", marketResponse).
+			Times(1).
+			Return(errors.New("oh no"))
+
+		err := service.GetEventMarkets(&req, server)
+
+		if err != nil {
+			t.Fatalf("Expected nil, got %s", err.Error())
+		}
+
+		assert.Equal(t, "error streaming market back to client: oh no", hook.LastEntry().Message)
+		assert.Equal(t, 1, len(hook.Entries))
+		assert.Equal(t, logrus.ErrorLevel, hook.LastEntry().Level)
+		reader.AssertExpectations(t)
+		server.AssertExpectations(t)
+	})
+}
+
 type MockMarketReader struct {
 	mock.Mock
 }
@@ -173,6 +423,16 @@ type MockExchangeOddsServer struct {
 }
 
 func (m *MockExchangeOddsServer) Send(e *statistico.ExchangeOdds) error {
+	args := m.Called(e)
+	return args.Error(0)
+}
+
+type MockMarketServer struct {
+	mock.Mock
+	grpc.ServerStream
+}
+
+func (m *MockMarketServer) Send(e *statistico.Market) error {
 	args := m.Called(e)
 	return args.Error(0)
 }
